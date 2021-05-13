adcfa2d944a084badfb9a94380cceb9fc11b07b0
David Zheng <davidzheng141@gmail.com>
2021-05-13 00:55:26 -0700

    finished setup to start doing player movement

---
+++ Project_Velocity/Assets/_Scripts/Player/MouseLook.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public float mouseSens = 100f;
+    public Transform playerBody;
+    float xRot = 0f;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSens * Time.fixedDeltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSens * Time.fixedDeltaTime;
+
+        xRot -= mouseY;
+        xRot = Mathf.Clamp(xRot, -90, 90);
+
+        transform.localRotation = Quaternion.Euler(xRot, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+
+    }
+}
