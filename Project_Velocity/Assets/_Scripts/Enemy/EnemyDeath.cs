03ffb97da87bb52e58151a926ca0f71a15595bd7
David Zheng <davidzheng141@gmail.com>
2021-05-19 17:07:42 -0700

    stuff

--- Project_Velocity/Assets/_Scripts/Enemy/EnemyDeath.cs
+++ Project_Velocity/Assets/_Scripts/Enemy/EnemyDeath.cs
@@ -4,11 +4,8 @@ using UnityEngine;
 
 public class EnemyDeath : MonoBehaviour
 {
-    [SerializeField] GameObject deathParticle;
-
     public void Die()
     {
-        Instantiate(deathParticle);
         Destroy(gameObject);
     }
 }
