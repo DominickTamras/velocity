8a296de9bb8772f461a32a47b783506a2fa59560
David Zheng <davidzheng141@gmail.com>
2021-07-11 02:32:56 -0700

    knees weak, arms are heavy

--- Project_Velocity/Assets/_Scripts/_Menus/LevelSelect.cs
+++ Project_Velocity/Assets/_Scripts/_Menus/LevelSelect.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelSelect : MonoBehaviour
 {
     public LevelDataSO lv1, lv2, lv3, lv4, lv5, lv6, lv7, lv8, lv9;
 
-
+    public void loadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
 }
