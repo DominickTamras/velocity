5b89f58bdc16bf93a27a9282d622cb2121d571d8
DominickTamras <31298279+DominickTamras@users.noreply.github.com>
2021-07-29 22:11:15 -0700

    Enemy models
    Replaced tmep models with kevins

--- Project_Velocity/Assets/_Scripts/Player/PlayerAnimManager.cs
+++ Project_Velocity/Assets/_Scripts/Player/PlayerAnimManager.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerAnimManager : MonoBehaviour
-{
+{   
+  
     [SerializeField] Animator anim;
     [SerializeField] Animator punchAnim;
     [SerializeField] Animator headBob;
